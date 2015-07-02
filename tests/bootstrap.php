a13be4bc7406725641fdcbff5bf798159444e32f
Michal Vysinsky <michal@vysinsky.cz>
2015-07-02 17:29:56 +0200

    Tests: fixed appending fileSuffix in testRule helper

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -17,13 +17,13 @@ function testRule(Babylon\Rules\Rule $rule, $fileSuffix = '')
 	$prophet = new Prophecy\Prophet();
 	$writer = $prophet->prophesize(Babylon\Writer::class);
 	$writer->writeFile(Argument::type('string'), Argument::type('string'))->will(function ($args) use ($ruleReflection, $fileSuffix) {
-		Tester\Assert::matchFile(__DIR__ . '/data/php5/' . $ruleReflection->getShortName() . $fileSuffix . '.php', $args[1]);
+		Tester\Assert::matchFile(__DIR__ . '/data/php5/' . $ruleReflection->getShortName() . ($fileSuffix ? '.' . $fileSuffix : '') . '.php', $args[1]);
 	});
 
 	$transpiler = new Babylon\Transpiler($writer->reveal(), [
 		$rule,
 	]);
 
-	$transpiler->transpile(new Babylon\File(__DIR__ . '/data/php7/' . $ruleReflection->getShortName() . $fileSuffix . '.php'));
+	$transpiler->transpile(new Babylon\File(__DIR__ . '/data/php7/' . $ruleReflection->getShortName() . ($fileSuffix ? '.' . $fileSuffix : '') . '.php'));
 	$prophet->checkPredictions();
 }
